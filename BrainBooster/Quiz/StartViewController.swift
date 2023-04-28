49cb2092d8680ff945348a41b71f3b35e54708af
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-28 20:15:22 +0300

    StartVC added

---
+++ BrainBooster/Quiz/StartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StartViewController.swift
+//  BrainBooster
+//
+//  Created by Vsevolod Lashin on 28.04.2023.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
