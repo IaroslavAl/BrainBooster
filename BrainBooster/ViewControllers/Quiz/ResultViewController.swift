7a031d0611a4b51b2cf3158ee4c8680d9a5f9642
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-28 20:50:20 +0300

    ResultVC code added

--- BrainBooster/ViewControllers/Quiz/ResultViewController.swift
+++ BrainBooster/ViewControllers/Quiz/ResultViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ResultViewController: UIViewController {
-
+final class ResultViewController: UIViewController {
+    
+    @IBOutlet private var resultLabel: UILabel!
+    @IBOutlet private var reloadButton: UIButton!
+    
+    var countOfRightAnswers: Int!
+    var countOfQuestions: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        resultLabel.text = """
+            Вы правильно ответили на
+            \(countOfRightAnswers ?? 0) из \(countOfQuestions ?? 0) вопросов! 😊
+            """
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        reloadButton.layer.cornerRadius = reloadButton.frame.height / 2
+    }
+    
+    @IBAction private func reloadButtonPressed() {
+        dismiss(animated: true)
+    }
 }
