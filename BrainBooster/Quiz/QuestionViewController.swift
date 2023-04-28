933c64dda71922808ad8db4f8f46f56d3d26309b
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-28 20:15:54 +0300

    QuestionVC added

---
+++ BrainBooster/Quiz/QuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionViewController.swift
+//  BrainBooster
+//
+//  Created by Vsevolod Lashin on 28.04.2023.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
